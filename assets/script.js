95932390a2e2a21b72e2329927997792568402da
Tim Velasquez <Timmyv1031@gmail.com>
2024-08-14 12:28:19 -0500

    added JavaScript Functions to project

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,47 @@
-const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
-	console.log(topics[x]);
+function listTopics() {
+	for (let x = 0; x < topics.length; x++) {
+		console.log(topics[x]);
+	}
 }
 
-// Good logic design. Reduce runtime. Made the intake process to uppercase.
-// switch (topic.toUpperCase()) {
-// 	case "HTML":
+// function selectTopic() {
+// 	if (randomTopic === "HTML") {
 // 		console.log("Let's study HTML!");
-// 		break;
-// 	case "CSS":
+// 	} else if (randomTopic === "CSS") {
 // 		console.log("Let's study CSS!");
-// 		break;
-// 	case "GIT":
+// 	} else if (randomTopic === "Git") {
 // 		console.log("Let's study Git!");
-// 		break;
-// 	case "JAVASCRIPT":
+// 	} else if (randomTopic === "JavaScript") {
 // 		console.log("Let's study JavaScript!");
-// 		break;
+// 	} else {
+// 		console.log("Please try again!");
+// 	}
 // }
+
+// Good logic design. Reduce runtime.
+function selectTopic() {
+	switch (randomTopic) {
+		case "HTML":
+			console.log("let's study HTML!");
+			break;
+		case "CSS":
+			console.log("let's study CSS!");
+			break;
+		case "Git":
+			console.log("let's study Git!");
+			break;
+		case "JavaScript":
+			console.log("let's study JavaScript!");
+			break;
+        default: 
+            console.log('Please try again!');
+	}
+} 
+
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
