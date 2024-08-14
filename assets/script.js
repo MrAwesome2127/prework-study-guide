cb6d9e4a3f9b2139d3b1e6baa75154724539fb20
Tim Velasquez <Timmyv1031@gmail.com>
2024-08-14 12:09:55 -0500

    added JavaScript to project

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let x = 0; x < topics.length; x++) {
+	console.log(topics[x]);
+}
+
+// Good logic design. Reduce runtime. Made the intake process to uppercase.
+// switch (topic.toUpperCase()) {
+// 	case "HTML":
+// 		console.log("Let's study HTML!");
+// 		break;
+// 	case "CSS":
+// 		console.log("Let's study CSS!");
+// 		break;
+// 	case "GIT":
+// 		console.log("Let's study Git!");
+// 		break;
+// 	case "JAVASCRIPT":
+// 		console.log("Let's study JavaScript!");
+// 		break;
+// }
